b05d8867ca161ab5de358a75b8f378e1aa2c1159
Swapnil89 <bari.swapnil89@gmail.com>
2020-07-17 19:18:56 +0530

    with lambdas in private subnet. need to debug db connection in public subnet

--- rds.tf
+++ rds.tf
@@ -30,7 +30,8 @@ resource "null_resource" "wait_for_db_create" {
 
 resource "aws_vpc" "aurora_db_vpc" {
   cidr_block = local.rds_vpc_cidr
-
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   tags = {
     Name = "aurora_db_vpc"
   }
--- vpc_endpoints.tf
+++ vpc_endpoints.tf
@@ -4,6 +4,9 @@ locals {
   public_auroa_vpc_cidr = "${aws_vpc.public_aurora_vpc.cidr_block}"
   public_aurora_vpc_id  = "${aws_vpc.public_aurora_vpc.id}"
   public_aurora_subnets = ["${aws_subnet.public_aurora_subnet.id}"]
+  aurora_db_vpc_cidr    = "${aws_vpc.aurora_db_vpc.cidr_block}"
+  aurora_db_vpc_id      = "${aws_vpc.aurora_db_vpc.id}"
+  aurora_db_subnets     = [ "${aws_subnet.aurora_db_subnet_2a.id}", "${aws_subnet.aurora_db_subnet_2b.id}" ]
   postgres_vpc_service  = "${aws_vpc_endpoint_service.aurora-example-postgresql-endpoint-service.service_name}"
   postgres_db_port      = "${module.aurora.this_rds_cluster_port}"
 }
@@ -56,14 +59,14 @@ resource "aws_vpc_endpoint" "postgres_endpoint" {
 resource "aws_security_group" "secret_manager_endpoint_sg" {
   name        = "Allow HTTPS"
   description = "Allow HTTPS"
-  vpc_id      = local.public_aurora_vpc_id
+  vpc_id      = local.aurora_db_vpc_id
 
   ingress {
     description = "Allow HTTPS connect"
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = [ local.public_auroa_vpc_cidr ]
+    cidr_blocks = [ local.aurora_db_vpc_cidr ]
   }
 
   egress {
@@ -79,8 +82,8 @@ resource "aws_security_group" "secret_manager_endpoint_sg" {
 }
 
 resource "aws_vpc_endpoint" "secret_manager_endpoint" {
-  vpc_id       = local.public_aurora_vpc_id
-  subnet_ids   = local.public_aurora_subnets
+  vpc_id       = local.aurora_db_vpc_id
+  subnet_ids   = local.aurora_db_subnets
   service_name = "com.amazonaws.us-east-2.secretsmanager"
   vpc_endpoint_type = "Interface"
   security_group_ids = [
